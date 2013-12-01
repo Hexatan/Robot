e4f2f12c20bb8982b2222192b5619f3cef18b82b
Hexatan <duvaux.gaetan@gmail.com>
2013-12-01 18:35:25 +0100

    Début du projet

---
+++ src/robot/InaccessibleCoordinate.java
@@ -0,0 +1,9 @@
+package robot;
+
+public class InaccessibleCoordinate extends Exception {
+    private Coordinates coordinate;
+
+    public InaccessibleCoordinate(Coordinates coordinate) {
+        this.coordinate = coordinate;
+    }
+}
---
+++ src/robot/Launcher.java
@@ -0,0 +1,7 @@
+package robot;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Robot r = new Robot();
+    }
+}
