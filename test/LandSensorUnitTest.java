0e633357b7bc19d780a9f657cdf15123b65a17a4
Hexatan <duvaux.gaetan@gmail.com>
2013-12-02 10:10:08 +0100

    Je sais créer des classes

---
+++ test/LandSensorUnitTest.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: gaetan
+ * Date: 02/12/13
+ * Time: 10:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class LandSensorUnitTest {
+}
