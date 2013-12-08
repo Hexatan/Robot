8383f05f03d1c265085e2a16969fff8cc21bd207
Fleurot Nicolas <nicolas3699@gmail.com>
2013-12-08 15:57:56 +0100

    Ajout des libs de test + test Batterie + test Coord

--- test/BatteryTestUnit.java
+++ test/BatteryTestUnit.java
@@ -5,5 +5,68 @@
  * Time: 10:06
  * To change this template use File | Settings | File Templates.
  */
+
+import org.junit.Test;
+import org.junit.Assert;
+import robot.Battery;
+import robot.InsufficientChargeException;
+
 public class BatteryTestUnit {
+
+    @Test
+    public void testGetCharge()
+    {
+        Battery b = new Battery();
+        Assert.assertEquals(b.getChargeLevel(), 100, 0);
+    }
+
+    @Test
+    public void testCharge()
+    {
+        Battery b = new Battery();
+        float chargeLevel = b.getChargeLevel();
+        b.charge();
+        Assert.assertTrue(b.getChargeLevel() > chargeLevel);
+    }
+
+    @Test
+    public void testUse()
+    {
+        Battery b = new Battery();
+        float chargeLevel = b.getChargeLevel();
+        try {
+            b.use(50);
+        } catch (InsufficientChargeException e) {
+            e.printStackTrace();
+        }
+        Assert.assertTrue(b.getChargeLevel() < chargeLevel);
+        try {
+            b.use(55);
+        } catch (InsufficientChargeException e) {
+            Assert.assertTrue(b.getChargeLevel() < 55);
+        }
+    }
+
+    @Test
+    public void testTimeToSufficientCharge()
+    {
+        Battery b = new Battery();
+        Assert.assertEquals(b.timeToSufficientCharge(100), 0);
+        Assert.assertEquals(b.timeToSufficientCharge(150), 5000);
+    }
+
+    @Test
+    public void testCanDeliver()
+    {
+        Battery b = new Battery();
+        Assert.assertTrue(b.canDeliver(10));
+        Assert.assertFalse(b.canDeliver(1000000));
+    }
+
+    @Test
+    public void testSetUp()
+    {
+        Battery b = new Battery();
+        // A faire si un jour je comprend comment ça marche.
+    }
 }
--- test/CoordinatesTestUnit.java
+++ test/CoordinatesTestUnit.java
@@ -5,5 +5,26 @@
  * Time: 10:07
  * To change this template use File | Settings | File Templates.
  */
+
+
+import org.junit.Test;
+import org.junit.Assert;
+import robot.Coordinates;
+
+
 public class CoordinatesTestUnit {
+
+    @Test
+    public void testEquals()
+    {
+        Coordinates c1 = new Coordinates(0,1);
+        Coordinates c2 = new Coordinates(0,1);
+        Coordinates c3 = new Coordinates(2,1);
+        String o = "01";
+        Assert.assertEquals(c1.getX(),0);
+        Assert.assertEquals(c1.getY(),1);
+        Assert.assertTrue(c1.equals(c2));
+        Assert.assertFalse(c1.equals(c3));
+        Assert.assertFalse(c1.equals(o));
+    }
 }
